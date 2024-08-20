a619348023af49084bc383a2361c3f5b5fd24437
paolmati <p.c.matias@icloud.com>
2024-08-19 22:12:53 -0400

    Coded cart quantity functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,12 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem, removeItem, updateQuantity } from "./CartSlice";
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -246,6 +248,17 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+        ...prevState,
+        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+    }));
+    };
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
