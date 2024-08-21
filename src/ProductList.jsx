9d9eedd29b9de9662745b7a2feb8265c50ffca40
paolmati <p.c.matias@icloud.com>
2024-08-20 21:34:34 -0400

    Added import from redux in ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem, removeItem, updateQuantity } from "./CartSlice";
+import  { useSelector, useDispatch } from 'react-redux';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
